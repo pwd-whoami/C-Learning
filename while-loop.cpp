830c95c237c5f58258a79ee0168f9abac28a6201
研 <143878614+pwd-whoami@users.noreply.github.com>
2026-08-12 16:54:44 -0400

    Create while-loop.cpp

---
+++ while-loop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//int x = 5;
+	string passed;
+	
+	while (passed != "yes")
+	{
+		cout<<"I will Passed this Sem?"<<endl;
+		getline(cin,passed);
+		
+		if (papasa == "yes")
+		{
+			cout<<"Yes i made it!\n"<<endl;
+		}
+		else 
+		{
+			cout<<"I Failed \n"<<endl;
+		}
+	}
+	
+	return  0;
+}
