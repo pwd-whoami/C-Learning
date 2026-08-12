5b0e9f53af09218a4efdabf5db02bf393027122d
研 <143878614+pwd-whoami@users.noreply.github.com>
2026-08-12 16:52:22 -0400

    Create Arranged.cpp

---
+++ Arranged.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	
+	cout<<"\nEnter a number:\t";
+	cin>>num;
+	
+	for(int i=num;i>=0;--i)
+	{
+		cout<<i<<endl;
+
+	}
+	 cout<<"\nThe Number Currently Arranged from Highest to lowest:"<<endl;
+	 
+	 return 0;
+
+	
+}
